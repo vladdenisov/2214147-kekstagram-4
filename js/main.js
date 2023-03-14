72e026cdb832aa6866e3b4ad44cff6395b9da6cf
Vlad Denisov <vlad.a.denisov@gmail.com>
2023-03-15 02:04:38 +0300

    add more descriptions

--- js/main.js
+++ js/main.js
@@ -14,6 +14,14 @@ function generateData() {
 
   const result = [];
 
+  const comments = [
+    'Посмотрите какая красота!',
+    'Ваууу!!',
+    'Блин, какая красота!',
+    'Супер фотка получилась!',
+    'Ну как прикольно вышло'
+  ];
+
   for (let i = 0; i < 25; i++) {
     let id = getRandomPositiveInteger(1, 25);
 
@@ -26,7 +34,7 @@ function generateData() {
     result.push({
       id,
       url: `photos/${id}.jpg`,
-      description: 'Test',
+      description: comments[getRandomPositiveInteger(0, comments.length - 1)],
       likes: getRandomPositiveInteger(15, 200),
       comments: getRandomPositiveInteger(0, 200)
     });
