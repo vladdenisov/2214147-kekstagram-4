050b4c4a662f0eae5f6e040d0b673cd94c368d3d
Vlad Denisov <ipovlad.yt@gmail.com>
2023-03-14 18:29:18 +0000

    Task 1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,32 @@ function getRandomPositiveInteger(a, b) {
 function checkStringLength(string, length) {
   return string.length <= length;
 }
+
+function generateData() {
+  const usedIds = [];
+
+  const result = [];
+
+  for (let i = 0; i < 25; i++) {
+    let id = getRandomPositiveInteger(1, 25);
+
+    while (id in usedIds) {
+      id = getRandomPositiveInteger(1, 25);
+    }
+
+    usedIds.push(id);
+
+    result.push({
+      id,
+      url: `photos/${id}.jpg`,
+      description: 'Test',
+      likes: getRandomPositiveInteger(15, 200),
+      comments: getRandomPositiveInteger(0, 200)
+    });
+  }
+  return result;
+}
+
+checkStringLength('Test');
+
+generateData();
